2ba8dc49adc23520c08918a344b503922900c22e
xihajun <jhuang10@ed.ac.uk>
2023-09-24 01:11:20 +0100

    Update api.py

--- fakeopenai/api.py
+++ fakeopenai/api.py
@@ -2,10 +2,16 @@ import requests
 
 api_key = 'pk-this-is-a-real-free-pool-token-for-everyone'  # Class attribute to hold the API key
 
+
 class ChatCompletion:
-    api_key = api_key
+    api_key = api_key  # Class attribute to hold the API key
+
     @classmethod
-    def create(cls, model, messages, max_tokens=1000):
+    def create(cls, model, messages, functions=None, function_call=None, 
+               temperature=None, top_p=None, n=None, stream=None, stop=None, 
+               max_tokens=None, presence_penalty=None, frequency_penalty=None, 
+               logit_bias=None, user=None):
+        
         if cls.api_key is None:
             raise Exception("API key not set. Please set it using ChatCompletion.api_key.")
         
@@ -13,11 +19,26 @@ class ChatCompletion:
             'Authorization': f'Bearer {cls.api_key}',
             'Content-Type': 'application/json'
         }
+        
         payload = {
             'model': model,
+            'messages': messages,  # Array of dictionaries with role, content, name, function_call
+            'functions': functions,  # Array of dictionaries with name, description, parameters
+            'function_call': function_call,
+            'temperature': temperature,
+            'top_p': top_p,
+            'n': n,
+            'stream': stream,
+            'stop': stop,
             'max_tokens': max_tokens,
-            'messages': messages
+            'presence_penalty': presence_penalty,
+            'frequency_penalty': frequency_penalty,
+            'logit_bias': logit_bias,
+            'user': user
         }
+        
+        # Remove None values from payload
+        payload = {k: v for k, v in payload.items() if v is not None}
 
         response = requests.post('https://ai.fakeopen.com/v1/chat/completions', headers=headers, json=payload)
         if response.status_code != 200:
